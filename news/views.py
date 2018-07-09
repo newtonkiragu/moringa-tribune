c2ff8676974cc30223329d8438ab376f53837681
newtonkiragu <karanunewton4@gmail.com>
2018-07-09 19:21:21 +0300

    restructured code to display the day's news

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 
@@ -23,42 +23,23 @@ def news_of_day(request):
             '''
     return HttpResponse(html)
 
-def convert_dates(dates):
-    """
-    function that gets the weekday number for the dates
-    """
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-    # returning the actual day of the weekday
-    day = days[day_number]
-
-    return day
+def news_today(request):
+    date = dt.date.today()
+    return render(request, 'all-news/today-news.html', {"date": date,})
 
 
-
-def past_days_news(request,past_date):
-    '''
-    function to display news from dates that have already passed
-    '''
+def past_days_news(request, past_date):
 
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
 
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_today)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
