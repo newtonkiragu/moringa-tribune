32c415712d2751e4b53441bc8315b6a10e6934fa
newtonkiragu <karanunewton4@gmail.com>
2018-07-09 18:39:57 +0300

    added function to convert dates and to display news of a certain date

--- news/views.py
+++ news/views.py
@@ -7,11 +7,41 @@ def welcome(request):
     return HttpResponse('Welcome to the Moringa Tribune')
 
 def news_of_day(request):
+    """
+    function that adds dynamic content(date) to the website
+    """
     date = dt.date.today()
     html = f'''
         <html>
             <body>
                 <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+    """
+    function that gets the weekday number for the dates
+    """
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    # returning the actual day of the weekday
+    day = days[day_number]
+
+    return day
+
+def news_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
