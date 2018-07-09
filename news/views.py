62bd2192eb0a80c3bd6222b179d1a99fddc701db
newtonkiragu <karanunewton4@gmail.com>
2018-07-09 11:55:19 +0300

    initial commit

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome to Moringa Tribune')
+    return HttpResponse('Welcome to the Moringa Tribune')
--- tribune/settings.py
+++ tribune/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'up7m57lhxqp_hu0@lf_@tq04)^-5=ga*f@#k@8ky*u3=oj$r!z'
+SECRET_KEY = '1_v%m7@or+x5%z=gi7e8lvewgj=r^n6ueli(bm@usxg^umu0p='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
