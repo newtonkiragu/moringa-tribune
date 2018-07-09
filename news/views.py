01501d34f62c2f079ebb968e1aa1b75ed75b7b05
newtonkiragu <karanunewton4@gmail.com>
2018-07-09 18:45:30 +0300

    fixed function to display news from archives

--- news/views.py
+++ news/views.py
@@ -56,3 +56,15 @@ def past_days_news(request,past_date):
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
